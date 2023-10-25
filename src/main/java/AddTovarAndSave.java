9c8794c8d90371a562dfcd2c10c1c60a3e3b8ef3
Vladi1989 <bboyvladi1989@mail.ru>
2023-10-25 12:24:30 +0300

    1-я практика исправлено (ввод на название и стоимость )

--- src/main/java/AddTovarAndSave.java
+++ src/main/java/AddTovarAndSave.java
@@ -1,14 +1,16 @@
 import java.util.ArrayList;
+import java.util.InputMismatchException;
 import java.util.List;
 import java.util.Scanner;
 
-public class AddTovarAndSave {
+class AddTovarAndSave  {
     List<TovarAndPrice> spisokTovarov;
 
-    public AddTovarAndSave(){
+    public AddTovarAndSave() {
         spisokTovarov = new ArrayList<>();
     }
-    public void addBludo(Scanner scanner){
+
+    public void addBludo (Scanner scanner) {
         System.out.println("Добавить блюдо или напиток: ");
         while (true) {
             System.out.println("Введите название товара (или нажмите 'Завершить'): ");
@@ -17,10 +19,26 @@ public class AddTovarAndSave {
             if (bludoName.equalsIgnoreCase("Завершить")) {
                 break;
             }
-            System.out.println("Введите стоимость товара: ");
-            double bludoPrice = scanner.nextDouble();
 
-            if (bludoPrice < 0){
+            if (!bludoName.matches("[a-zA-Zа-яА-Я]+")) {
+                System.out.println("У нас в ресторане все названия блюд пишутся буквами, введите еще раз: ");
+                continue;
+            }
+
+            double bludoPrice = -1;
+            boolean noPrice = false;
+            while (!noPrice) {
+                System.out.println("Введите цену: ");
+                try {
+                    bludoPrice = scanner.nextDouble();
+                    noPrice = bludoPrice >= 0;
+                } catch (InputMismatchException e) {
+                    System.out.println("Ввод осуществляется только цифрами, попробуйте еще раз");
+                    scanner.next();
+                }
+            }
+
+            if (bludoPrice < 0) {
                 System.out.println("Похоже вы ошиблись с вводом, попробуйте еще раз" +
                         "а то вы хотите, чтоб за товар заплатили ВАМ:-) ");
                 continue;
@@ -29,10 +47,8 @@ public class AddTovarAndSave {
             System.out.println("Товар добавлен");
         }
     }
-
-    public double getAllSumma(){
+    public double getAllSumma() {
         return spisokTovarov.stream().mapToDouble(TovarAndPrice::getTovarPrice).sum();
-
     }
     public List<TovarAndPrice> getAddedTovars() {
         return spisokTovarov;
